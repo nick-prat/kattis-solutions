395e2af392efcbeeafd5d0e6b4d7259fa9411efe
Nicholas Prat <nprat@protonmail.com>
2020-04-18 01:58:24 -0400

    Finished gnoll hypothesis

--- gnoll_hypothesis.py
+++ gnoll_hypothesis.py
@@ -4,22 +4,18 @@ N, K = [int(v) for v in sys.stdin.readline().strip('\n').split(" ")]
 S = [float(s) for s in sys.stdin.readline().strip('\n').split(" ")]
 
 # ! Tied for fastest python3 solution on kattis as of 04/18/2020 01:32 AM, 0.07s
-A = []
-for I in range(N):
-    if K == 0:
-        A.append(float(0))
-        continue
 
-    P = 0
-    R = 1
-    for J in range(N - K + 1):
-        if J == 0:
-            R = R * (K / N)
-        else:
-            R = R * ((N - K - J + 1) / (N - J))
-        P = P + R * S[(I - J) % N]
-    A.append(P)
-
-for a in A:
-    print("{}".format(a), end=' ')
+if K == 0:
+    for I in range(N):
+        print("0.0", end = ' ')
+else:
+    D = N - K
+    Q = K / N
+    for I in range(N):
+        R = Q
+        P = R * S[I]
+        for J in range(1, D + 1):
+            R *= ((D - J + 1) / (N - J))
+            P += R * S[(I - J) % N]
+        print(P, end = ' ')
 print()
